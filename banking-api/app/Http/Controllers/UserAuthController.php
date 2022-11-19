048a8e47e2ff0f56d1644a61ddbe1ad625641612
andrew21-mch <nfonandrew73@gmail.com>
2022-11-19 06:50:30 +0100

    authentication and security

---
+++ banking-api/app/Http/Controllers/UserAuthController.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Branch;
+use App\Models\Account;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+
+class UserAuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8'],
+            'phone' => ['required', 'string', 'max:255'],
+            'city' => ['required', 'string', 'max:255'],
+            'country' => ['required', 'string', 'max:255'],
+            'address' => ['required', 'string', 'max:255'],
+            'dob' => ['required', 'date'],
+            'branch_code' => ['required', 'integer', 'max:255'],
+            'create_account' => ['required', 'boolean'],
+
+        ]);
+
+        // if create account is true, make sure account number is unique
+        if($request->create_account == true){
+            $validator = Validator::make($request->all(), [
+                'account_type' => ['required', 'string', 'max:255'],
+            ]);
+        }
+
+
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+
+        try {
+
+            $user = new User;
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+            $user->phone = $request->phone;
+            $user->city = $request->city;
+            $user->country = $request->country;
+            $user->address = $request->address;
+            $user->dob = $request->dob;
+            $user->branch_code = $request->branch_code;
+            $user->save();
+
+            $user->assignRole('customer');    
+
+            $branch = Branch::find($request->branch_code);
+
+            // create user account
+            $account = new Account;
+            $account->account_num = $this->generateAccountNumber($branch, $user);
+            $account->type = $request->account_type;
+            $account->balance = 0;
+            $account->user_id = $user->id;
+
+            $user->accounts()->save($account);
+
+            return $this->userResponse($user);
+
+        } catch (\Exception $e) {
+            $user->delete();
+            $account->delete();
+            return response()->json(['message' => $e], 409);
+        }
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' =>['required', 'string', 'email', 'max:100'],
+            'password' => ['required', 'string', 'min:6'],
+        ]);
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user) {
+            return response()->json([
+                'message' => 'User not found'
+            ], 404);
+        }
+
+        if (Hash::check($request->password, $user->password)) {
+
+            // create api token
+            $token = $user->createToken('authToken')->plainTextToken;
+            return response()->json([
+                'token' => $token
+            ], 200);
+        } else {
+            $response = "Password missmatch";
+            return response($response, 422);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+        $response = 'You have been succesfully logged out!';
+        return response($response, 200);
+    }
+
+    public function userResponse($user)
+    {
+        $user->load('roles:name');
+        $user->branch = Branch::find($user->branch_code);
+        return response()->json($user);
+    }
+
+    public function updateRole(Request $request)
+    {
+        // validate incoming request
+        $validator = Validator::make($request->all(), [
+            'id' => ['required', 'integer'],
+            'role' => ['required', 'string', '12'],
+        ]);
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        try {
+            $user = User::find($request->id);
+            $user->syncRoles($request->role);
+            return $this->userResponse($user);
+        } catch (\Exception $e) {
+            //return error message
+            return response()->json(['message' => $e], 409);
+        }
+    }
+
+    private function generateAccountNumber($branch, $user)
+    {
+        $accountNumber = $branch->branch_code . $user->id;
+        return $accountNumber;
+    }
+
+}
